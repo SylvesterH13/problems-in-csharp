be390e920f625e63da9852c51a3a1cafd8ff4263
SylvesterH13 <henrique.sylvester@gmail.com>
2022-10-12 23:43:44 -0300

    Add project files.

---
+++ ProblemsInCSharp/KArraySizeGreatestSum/InputData.cs
@@ -0,0 +1,18 @@
+﻿namespace KArraySizeGreatestSum
+{
+    public static class InputData
+    {
+        public static IEnumerable<(int[] elements, int k)> Get()
+        {
+            yield return (elements: new int[] { 1, 2, 3, 4 }, k: 1);
+            yield return (elements: new int[] { 1, 2, 3, 4 }, k: 2);
+            yield return (elements: new int[] { 1, 2, 3, 4 }, k: 3);
+            yield return (elements: new int[] { 1, 2, 3, 4 }, k: 4);
+
+            yield return (elements: new int[] { 2, 4, 3, 1 }, k: 1);
+            yield return (elements: new int[] { 2, 4, 3, 1 }, k: 2);
+            yield return (elements: new int[] { 2, 4, 3, 1 }, k: 3);
+            yield return (elements: new int[] { 2, 4, 3, 1 }, k: 4);
+        }
+    }
+}
---
+++ ProblemsInCSharp/KArraySizeGreatestSum/Program.cs
@@ -0,0 +1,40 @@
+﻿using KArraySizeGreatestSum;
+
+var GetKArraySizeGreatestSum = (int[] elements, int k) =>
+{
+    if (k > elements.Length)
+    {
+        throw new ArgumentException($"The sub array length cannot be greater then {nameof(elements)} length", nameof(k));
+    }
+
+    var firstSubArraySum = 0;
+
+    for (var i = 0; i < k; i++)
+    {
+        firstSubArraySum += elements[i];
+    }
+
+    var greatestSum = firstSubArraySum;
+    var lastSum = firstSubArraySum;
+    for (int i = 1, j = k; j < elements.Length; j++, i++)
+    {
+        var minusOneIndexElement = elements[i - 1];
+        var lastElementCurrentSubArray = elements[j];
+
+        var currentSum = lastSum - minusOneIndexElement + lastElementCurrentSubArray;
+        if (greatestSum < currentSum)
+        {
+            greatestSum = currentSum;
+        }
+
+        lastSum = currentSum;
+    }
+
+    return greatestSum;
+};
+
+foreach ((int[] elements, int k) in InputData.Get())
+{
+    var result = GetKArraySizeGreatestSum(elements, k);
+    Console.WriteLine($"elements: [{string.Join(", ", elements)}]; k: {k}; result: {result}");
+}
